07ef7eb12f1a29523374095b9b8a598b9fb6bcda
chrislose23 <chris@Christophers-MacBook-Air.local>
2024-04-25 14:54:32 -0400

    added 4 questions to the questions variable

--- index.js
+++ index.js
@@ -3,7 +3,34 @@ const colors = require('colors');
 const fs = require('fs');
 const shapes = require('./lib/shapes.js');
 
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter text (up to 3 characters):',
+        validate: function(value) {
+            return value.length > 0 && value.length <= 3 ? true : 'Please enter up to 3 characters.';
+        }
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter text color:',
+        default: 'white'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose shape:',
+        choices: ['circle', 'triangle', 'square']
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter shape color:',
+        default: 'blue'
+    }
+];
 
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) => {
@@ -16,7 +43,7 @@ function writeToFile(fileName, data) {
 }
 
 function init() {
-    
+
 }
 
 init();
